1a23b9e868af0b5faac3fbb85a87b5bcd6fa612f
Andrew <a.zolotorevskii@g.nsu.ru>
2025-09-01 13:04:29 +0700

    rename configs

--- src/main/java/ru/zolo/config/quartz/QuartzConfig.java
+++ src/main/java/ru/zolo/config/quartz/QuartzConfig.java
@@ -1,84 +1,58 @@
 package ru.zolo.config.quartz;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.RequiredArgsConstructor;
-import org.quartz.CronExpression;
-import org.quartz.CronScheduleBuilder;
-import org.quartz.Job;
-import org.quartz.JobDetail;
-import org.quartz.Trigger;
-import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
+import org.quartz.TriggerListener;
+import org.quartz.utils.ConnectionProvider;
+import org.quartz.utils.DBConnectionManager;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import ru.zolo.properties.ScheduleProperties;
-import ru.zolo.schedule.JobSchedulerHelper;
-import ru.zolo.schedule.jobs.JobBase;
+import ru.zolo.config.quartz.listiner.QuartzTriggerListener;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Properties;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Configuration
 @RequiredArgsConstructor
 public class QuartzConfig {
-    private final ScheduleProperties scheduleProperties;
-
     @Bean
-    public SchedulerFactoryBeanCustomizer schedulerFactoryBeanCustomizer(List<Job> jobs) {
-        return factory -> {
-            List<JobDetail> jobDetails = new ArrayList<>();
-            List<Trigger> triggers = new ArrayList<>();
-
-            for (Job job2 : jobs) {
-                if (JobBase.class.isAssignableFrom(job2.getClass())) {
-                    JobBase job = (JobBase) job2;
-                    Optional.ofNullable(scheduleProperties.getJobs().get(job.getBeanName())).ifPresent(jobConfig -> {
-                        String cron = jobConfig.getCron();
-                        if (cron != null && CronExpression.isValidExpression(cron)) {
-                            //todo
-                            //  Дано:
-                            //      Cron: "0/5 * * * * ?" (каждые 5 сек)
-                            //      Выполнение: 1-7 сек
-                            //      Все потоки заняты в момент 12:00:00
-                            //  Идеал:
-                            //      Потоков нет → запуск откладывается
-                            //      Потоки есть \ задача завершена -> запуск в 12 00 00
-                            //      Потоки есть задача не завершена - > запуск в 12 00 05
-                            CronScheduleBuilder cronExpression = CronScheduleBuilder.cronSchedule(cron)
-//                                    .withMisfireHandlingInstructionFireAndProceed();
-//                                    .withMisfireHandlingInstructionIgnoreMisfires();
-                                    .withMisfireHandlingInstructionDoNothing();
-
-
-                            Class<? extends Job> jobClass = job.getClass();
-
-                            JobDetail jobDetail = JobSchedulerHelper.buildJobDetail(jobClass, job.getBeanName());
-                            Trigger trigger = JobSchedulerHelper.buildCronTrigger(jobDetail, job.getBeanName(), cronExpression);
-
-                            jobDetails.add(jobDetail);
-                            triggers.add(trigger);
-                        }
-                    });
-                }
-            }
-
-            factory.setJobDetails(jobDetails.toArray(new JobDetail[0]));
-            factory.setTriggers(triggers.toArray(new Trigger[0]));
-            setProperty(factory, "org.quartz.threadPool.threadCount", Integer.valueOf(triggers.size() + 1/*кол-во заранее поднятных мб*/).toString());
-        };
+    public TriggerListener quartzTriggerListenerCreate() {
+        return new QuartzTriggerListener();
     }
 
-    private static void setProperty(SchedulerFactoryBean factory, String name, String value) {
-        try {
-            Field fieldQuartzProperties = SchedulerFactoryBean.class.getDeclaredField("quartzProperties");
-            fieldQuartzProperties.setAccessible(true);
-            Properties prop = (Properties) fieldQuartzProperties.get(factory);
-            prop.put(name, value);
-            fieldQuartzProperties.setAccessible(false);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+    @Bean
+    @ConfigurationProperties("datasource-quartz")
+    DataSourceProperties quartzDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean("quartzDataSource")
+    @ConfigurationProperties("datasource-quartz.configuration")
+    @QuartzDataSource
+    public DataSource quartzDataSource() {
+        return quartzDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @PostConstruct
+    public void registerQuartzConnectionProvider() {
+        DBConnectionManager.getInstance().addConnectionProvider("quartzDataSource", new ConnectionProvider() {
+            @Override
+            public Connection getConnection() throws SQLException {
+                return quartzDataSource().getConnection();
+            }
+
+            @Override
+            public void shutdown() {
+            }
+
+            @Override
+            public void initialize() {
+            }
+        });
     }
 }
